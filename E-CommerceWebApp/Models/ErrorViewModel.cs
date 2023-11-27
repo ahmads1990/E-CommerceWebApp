4d17c91e1075f7de287e0e2507197a6ad3fa4845
ahmads1990 <ahmads20002000@gmail.com>
2023-11-27 16:07:17 +0200

    Add project files.

---
+++ E-CommerceWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace E_CommerceWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
