048f7cf1fd787ceadde88f2fdfa23b8a09a07cac
Ahmad Khaled <ahmads20002000@gmail.com>
2023-12-23 08:13:44 +0200

    Added extrenal identity provider Google and changed user allowed nulls

--- E-CommerceWebApp/Models/ApplicationUser.cs
+++ E-CommerceWebApp/Models/ApplicationUser.cs
@@ -4,7 +4,7 @@
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public DateTime BirthDate { get; set; }
-        public bool GenderIsMale { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public bool? GenderIsMale { get; set; }
     }
 }
--- E-CommerceWebApp/Program.cs
+++ E-CommerceWebApp/Program.cs
@@ -4,6 +4,14 @@ using E_CommerceWebApp.Services;
 using E_CommerceWebApp.Models.Email;
 using System.Configuration;
 using Microsoft.AspNetCore.Identity.UI.Services;
+using Microsoft.Extensions.Options;
+using Microsoft.AspNetCore.Authentication;
+using System;
+using System.Security.Claims;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.PeopleService.v1;
+using Google.Apis.Services;
+using Google.Apis.PeopleService.v1.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -37,6 +45,60 @@ builder.Services.AddAuthorization(options =>
 {
     options.AddPolicy(CustomPolicies.canAccessAdmin,
         policyBuilder => policyBuilder.RequireClaim(CustomClaims.IsAdmin));
+});
+
+// Third party login
+// Google
+
+builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+{
+    googleOptions.ClientId = builder.Configuration["Authentication:Google:ClientId"];
+    googleOptions.ClientSecret = builder.Configuration["Authentication:Google:ClientSecret"];
+    // scope for birthday
+    googleOptions.Scope.Add("https://www.googleapis.com/auth/user.birthday.read");
+    googleOptions.Scope.Add("https://www.googleapis.com/auth/user.gender.read");
+    googleOptions.Scope.Add("https://www.googleapis.com/auth/user.phonenumbers.read");
+    googleOptions.SaveTokens = true;
+
+    googleOptions.Events.OnCreatingTicket = ctx =>
+    {
+        List<AuthenticationToken> tokens = ctx.Properties.GetTokens().ToList();
+
+        // start requset using google people service to get more info about the user (birthdate, gender)
+        // create people service with GoogleCredentials
+        var cred = GoogleCredential.FromAccessToken(tokens.First().Value);
+        var peopleServiceService = new PeopleServiceService(
+            new BaseClientService.Initializer() { HttpClientInitializer = cred });
+
+        // use people/me to access data for the current authenticated user 
+        var personRequest = peopleServiceService.People.Get("people/me");
+        // add requested fields
+        personRequest.PersonFields = "birthdays,genders";
+        // send request
+        Person person = personRequest.Execute();
+
+        // extracte values from response
+        var dateResponse = person.Birthdays.FirstOrDefault()?.Date;
+        DateTime? birthdateValue = null;
+        if (dateResponse.Day != null && dateResponse.Month != null && dateResponse.Year != null)
+            birthdateValue = new DateTime(dateResponse?.Year ?? 1, dateResponse?.Month ?? 1, dateResponse?.Day ?? 1);
+
+        var genderValue = person.Genders?.FirstOrDefault()?.FormattedValue;
+
+        // get user identity to add the claims too
+        var claimsIdentity = ctx.Principal?.Identities.First();
+        // create claims
+        claimsIdentity?.AddClaim(new Claim(ClaimTypes.Gender, genderValue ?? ""));
+        claimsIdentity?.AddClaim(new Claim(ClaimTypes.DateOfBirth, birthdateValue?.ToString() ?? "", ClaimValueTypes.DateTime));
+
+        tokens.Add(new AuthenticationToken()
+        {
+            Name = "TicketCreated",
+            Value = DateTime.UtcNow.ToString()
+        });
+        ctx.Properties.StoreTokens(tokens);
+        return Task.CompletedTask;
+    };
 });
 
 var app = builder.Build();
