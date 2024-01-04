d6ddd677b9fa2c9f85f3254c9237ea3fb28c74fc
ahmads1990 <ahmads20002000@gmail.com>
2024-01-04 22:04:34 +0200

    Removed seed data

--- E-CommerceWebApp/Services/ApplicationDBContext.cs
+++ E-CommerceWebApp/Services/ApplicationDBContext.cs
@@ -17,15 +17,6 @@ namespace E_CommerceWebApp.Services
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<Product>().HasData(
-             new Product { ProductImage = null, ProductID = 1, ProductName = "Fancy Product", Description = "Fancy", Price = 40 },
-             new Product { ProductImage = null, ProductID = 2, ProductName = "Special Item", Description = "Special", Price = 80 },
-             new Product { ProductImage = null, ProductID = 3, ProductName = "Sale Item", Description = "Sale", Price = 20 },
-             new Product { ProductImage = null, ProductID = 4, ProductName = "Popular Item", Description = "Popular", Price = 100 }
-             );
-            modelBuilder.Entity<Category>().HasData(
-            new Category { CategoryID = 1, CategoryName = "TestCat" }
-            );
             // Configure the ImageData property to be stored as binary data
             //modelBuilder.Entity<ProductImage>()
             //    .Property(e => e.ImageData)
